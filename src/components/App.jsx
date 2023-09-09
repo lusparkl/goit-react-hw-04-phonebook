6b4511bb739a45ab775c98f17c5c7cacc9fcaabb
lusparkl <flypyfl@gmail.com>
2023-09-09 14:25:30 +0300

    remake into hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,31 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { AddContactForm } from "./AddContactForm/AddContactForm";
 import { ContactsGroup } from "./ContactsGroup/ContactsGroup";
 import { FilterContacts } from "./Filter/Filter";
 
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: ''
-  }
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    this.setState({
-      contacts: contacts ? JSON.parse(contacts) : []
-    })
+  useEffect(() => {
+    const contactsFromStorage = localStorage.getItem('contacts');
+    setContacts(contactsFromStorage ? JSON.parse(contactsFromStorage) : []);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+
+  const onAppendToContacts = contact => {
+    if (isPossiblyToAppend(contact.name)) {
+      setContacts([...contacts, contact])
+    };
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contact || this.state.contacts !== null) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  };
-
-  onAppendToContacts = contact => {
-    if (this.isPossiblyToAppend(contact.name)) this.setState(prevState => ({
-      contacts: [...prevState.contacts, contact]
-    }));
-  };
-  
-  isPossiblyToAppend = name => {
-    for (const contact of this.state.contacts) {
+  const isPossiblyToAppend = name => {
+    for (const contact of contacts) {
       if (name.toLowerCase() === contact.name.toLowerCase()) {
         alert(`${name} is already in your contacts.`);
         return false;
@@ -39,32 +34,92 @@ export class App extends Component {
     return true;
   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id)
-    }));
+  const deleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
   };
 
-  findContact = searchQuery => {
-    this.setState({ filter: searchQuery.toLowerCase() });
+  const findContact = searchQuery => {
+    setFilter(searchQuery.toLowerCase())
   };
-  
-  render() {
-    const displayedContacts = this.state.filter
-      ? this.state.contacts.filter(contact =>
-        contact.name.toLowerCase().includes(this.state.filter)
-      )
-      : this.state.contacts; 
 
-    return (
-      <>
-        <AddContactForm addContact={this.onAppendToContacts} />
-        <FilterContacts findContact={this.findContact} />
-        <ContactsGroup
-          contacts={displayedContacts}
-          deleteContact={this.deleteContact}
-        />
-      </>
-    );
-  }
+  const displayedContacts = filter
+    ? contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter)
+    )
+    : contacts;
+
+  return (
+    <>
+      <AddContactForm addContact={onAppendToContacts} />
+      <FilterContacts findContact={findContact} />
+      <ContactsGroup
+        contacts={displayedContacts}
+        deleteContact={deleteContact}
+      />
+    </>
+  );
 }
+// export class App extends Component {
+//   state = {
+//     contacts: [],
+//     filter: ''
+//   }
+
+//   componentDidMount() {
+//     const contacts = localStorage.getItem('contacts');
+//     this.setState({
+//       contacts: contacts ? JSON.parse(contacts) : []
+//     })
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.contacts !== prevState.contact || this.state.contacts !== null) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   };
+
+//   onAppendToContacts = contact => {
+//     if (this.isPossiblyToAppend(contact.name)) this.setState(prevState => ({
+//       contacts: [...prevState.contacts, contact]
+//     }));
+//   };
+
+//   isPossiblyToAppend = name => {
+//     for (const contact of this.state.contacts) {
+//       if (name.toLowerCase() === contact.name.toLowerCase()) {
+//         alert(`${name} is already in your contacts.`);
+//         return false;
+//       }
+//     }
+//     return true;
+//   };
+
+//   deleteContact = id => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(contact => contact.id !== id)
+//     }));
+//   };
+
+//   findContact = searchQuery => {
+//     this.setState({ filter: searchQuery.toLowerCase() });
+//   };
+
+//   render() {
+//     const displayedContacts = this.state.filter
+//       ? this.state.contacts.filter(contact =>
+//         contact.name.toLowerCase().includes(this.state.filter)
+//       )
+//       : this.state.contacts;
+
+//     return (
+//       <>
+//         <AddContactForm addContact={this.onAppendToContacts} />
+//         <FilterContacts findContact={this.findContact} />
+//         <ContactsGroup
+//           contacts={displayedContacts}
+//           deleteContact={this.deleteContact}
+//         />
+//       </>
+//     );
+//   }
+// }
